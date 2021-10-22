cda74974b9393f1f82a6353105a5c80be31f4046
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2021-10-22 18:35:34 +0300

    Added refs

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+  searchForm: document.getElementById('search-form'),
+  galleryList: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+  labelBtn: document.querySelector('.label'),
+};
